16039fe92004231b962ad7dd29b7a4ba5332ac7b
Michael Verbeek <michael.verbeek@digitalsurvivalcompany.com>
2020-07-22 12:59:59 +0200

    Added EKS cluster support

---
+++ eks.py
@@ -0,0 +1,18 @@
+from ..utils import BasicSegment
+import os, re
+
+
+class Segment(BasicSegment):
+    def add_to_powerline(self):
+        k8s_glyph="\u2388"
+        eks = os.environ.get("KUBECONFIG") 
+        if eks:
+            try:
+                f = open(os.environ.get("KUBECONFIG"), "r")
+                cluster=re.search('(?<=name: ).*',f.read(), re.IGNORECASE).group()
+                self.powerline.append(" {} {} ".format(k8s_glyph,cluster),
+                                  self.powerline.theme.AWS_PROFILE_FG,
+                                  self.powerline.theme.AWS_PROFILE_BG)
+            except:
+                cluster=""
+            
