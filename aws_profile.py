9a11a3780fc05cf0e14b0ebee746504c08bff464
Michael Verbeek <49558899+MikeySoftNL@users.noreply.github.com>
2020-08-15 16:16:32 +0200

    Update AWS Profile, updated DEFAULT THEME Color

--- aws_profile.py
+++ aws_profile.py
@@ -9,7 +9,10 @@ class Segment(BasicSegment):
         aws_region = os.environ.get("AWS_REGION") or \
             os.environ.get("AWS_DEFAULT_REGION")
         if aws_profile:
-            aws_text = "{} | \uf454 {}".format(os.path.basename(aws_profile),aws_region)
+            if aws_region == None:
+                aws_text = "{}".format(os.path.basename(aws_profile))
+            else:
+                aws_text = "{} | \uf484 {}".format(os.path.basename(aws_profile),aws_region)
             self.powerline.append(" \ue7ad %s " % aws_text,
                                   self.powerline.theme.AWS_PROFILE_FG,
                                   self.powerline.theme.AWS_PROFILE_BG)
