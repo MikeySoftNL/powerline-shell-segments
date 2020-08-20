cc19014b2583626d8b97507cc476c32a9d87c2fd
Michael Verbeek <michael.verbeek@digitalsurvivalcompany.com>
2020-08-20 09:33:27 +0200

    Update Environment segment

--- environment.py
+++ environment.py
@@ -4,10 +4,10 @@ import os
 
 class Segment(BasicSegment):
     def add_to_powerline(self):
-        AWS_GLYPH = "\ue62e"
-        aws_profile = os.environ.get("ENVIRONMENT") or \
+        ENV_GLYPH = "\ue62e"
+        ENVIRONMENT = os.environ.get("ENVIRONMENT") or \
             os.environ.get("ENVIRONMENT")
-        if aws_profile:
-            self.powerline.append(" {} {} ".format(AWS_GLYPH, os.path.basename(aws_profile)) ,
-                                  self.powerline.theme.ENV_FG,
-                                  self.powerline.theme.ENV_BG)
+        if ENVIRONMENT:
+            self.powerline.append(" {} {} ".format(ENV_GLYPH, ENVIRONMENT) ,
+                self.powerline.theme.ENV_FG,
+                self.powerline.theme.ENV_BG)
